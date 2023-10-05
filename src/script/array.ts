9d5852d1178ffcaef15cebaaa6fc66b17070a8d9
TuPVKozocom <tupv@kozo-japan.com>
2023-10-05 22:12:29 +0700

    Khai báo type array

---
+++ src/script/array.ts
@@ -0,0 +1,22 @@
+const hobbies: string[] = ["badminton", "Coding", "Soccer"];
+
+const scores: number[] = [1, 2, 3, 4, 5];
+
+const filter: boolean[] = [true, false];
+
+const students: {
+  name: string;
+  age: number;
+  score: number;
+}[] = [
+  {
+    name: "Tu",
+    age: 20,
+    score: 10,
+  },
+  {
+    name: "Vu",
+    age: 19,
+    score: 20,
+  },
+];
