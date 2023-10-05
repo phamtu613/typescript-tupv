0e6b5461e66ed6b42c8734117822b3a9ccd0acaa
TuPVKozocom <tupv@kozo-japan.com>
2023-10-05 21:55:37 +0700

    Các type cơ bản

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-import "./style.css";
+const language: string = "javascript";
+const score: number = 100;
+const isAvailable: boolean = true;
+const text: string =
+  language === "javascript" ? "javascript" : "not javascript";
+let keyboard: any = undefined;
+keyboard = true;
+
+let study: unknown;
+study = "javascript";
+
+if (typeof study === "string") {
+  study.split("");
+}
+
+function sum(a: unknown, b: unknown) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }
+  return 0;
+}
+sum("10", 20);
