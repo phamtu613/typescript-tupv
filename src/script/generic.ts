5bff6929e804c24c01fb36f5d9ccbd83dbb2b83a
TuPVKozocom <tupv@kozo-japan.com>
2023-10-07 15:19:39 +0700

    constraint trong generic type

--- src/script/generic.ts
+++ src/script/generic.ts
@@ -14,3 +14,27 @@ pair(10, 20);
 pair(10, "20");
 pair(10, true);
 pair(10, [1, 2, 3]);
+
+// item se có object .length trả về number
+function logLength<T extends { length: number }>(item: T): void {
+  console.log(item.length);
+}
+
+console.log(logLength([1, 1, 1]));
+
+const user = {
+  fullName: "TuPV",
+  age: 20,
+  address: "Da nang",
+};
+
+type typeUser = typeof user;
+
+// O là type generic roi
+function logObjectValue<O extends {}, K extends keyof O>(obj: O, key: K): void {
+  console.log(obj[key]);
+}
+
+logObjectValue(user, "address");
+
+type Example = Record<string, any>;
